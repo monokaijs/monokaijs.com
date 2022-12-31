735caccad93d7b8ec1398ded67c6ba71b3a8fd43
Edward Nguyen <monokaijs@gmail.com>
2022-12-31 10:41:40 +0700

    ✨ ui for post item

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import {Divider} from "../components/shared/Divider";
 import {AuthorBio} from "../components/app/AuthorBio";
+import {PostList} from "../components/app/PostList";
 
 
 export default function Home() {
   return (
     <div>
       <AuthorBio/>
+      <div className={'content-container'}>
+        <div className={'col-left'}>
+          <h3>
+            Recent Posts
+          </h3>
+          <PostList/>
+        </div>
+        <div className={'col-right'}>
+
+        </div>
+      </div>
     </div>
   )
 }
