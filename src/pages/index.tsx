164e7493b7f4ab0d7dd0ee537b1427e85c8adbb8
Edward Nguyen <monokaijs@gmail.com>
2023-01-01 01:48:41 +0700

    ✨ add spotlight section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import {AuthorBio} from "../components/app/AuthorBio";
-import {PostList} from "../components/app/PostList";
+import {Card, Text} from "@nextui-org/react";
 
 
 export default function Home() {
   return (
     <div>
       <AuthorBio/>
+      <div>
+        <Text h3 weight={'bold'}>
+          Spotlight Projects
+        </Text>
+      </div>
+      <Card
+        isHoverable variant={'bordered'}
+        isPressable
+        css={{
+          background: 'transparent',
+          marginTop: '24px'
+        }}
+      >
+        <Card.Body>
+          <Text>A pressable card.</Text>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
