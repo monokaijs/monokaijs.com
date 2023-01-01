4940968c91ec3d199758e1ae32bc6934d4344a88
MonokaiJs <monokaijs@gmail.com>
2023-01-02 01:18:27 +0700

    💄 add home projects style

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -11,3 +11,21 @@ export const BlogLinks = [{
   title: 'Tools',
   path: '/tools'
 }];
+
+
+export const SpotlightProjects = [{
+  name: 'MonokaiToolkit',
+  description: 'MonokaiToolkit is a powerful toolset for Facebook users.',
+  icon: 'https://monokaitoolkit.com/_next/image?url=%2Fassets%2Fimages%2Flogo.png&w=128&q=75',
+  url: 'https://monokaitoolkit.com',
+}, {
+  name: 'SoundSpace',
+  description: 'Bring your music everywhere, without restrictions.',
+  icon: 'https://soundspace.app/assets/svgs/logo.svg',
+  url: 'https://soundspace.app',
+}, {
+  name: 'Squadev',
+  description: 'Monitoring your team, manage tasks & tracking project',
+  icon: '',
+  url: 'https://squadev.com',
+}]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,58 @@
 import {AuthorBio} from "../components/app/AuthorBio";
-import {Card, Text} from "@nextui-org/react";
+import {Card, Grid, Text} from "@nextui-org/react";
+import { SpotlightProjects } from "../configs/app.config";
+import styles from '../styles/Home.module.css';
+import Image from "next/image";
+import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowRight} from "@fortawesome/pro-solid-svg-icons";
 
 
 export default function Home() {
-  return (
+  let div = <>
     <div>
       <AuthorBio/>
-      <div>
+      <div className={styles.sectionHeader}>
         <Text h3 weight={'bold'}>
           Spotlight Projects
         </Text>
+        <Link href={'/projects'}>
+          <Text small css={{color: '$accents6'}}>
+            View all projects
+            <FontAwesomeIcon icon={faArrowRight} style={{marginLeft: 8}}/>
+          </Text>
+        </Link>
       </div>
-      <Card
-        isHoverable variant={'bordered'}
-        isPressable
-        css={{
-          background: 'transparent',
-          marginTop: '24px'
-        }}
-      >
-        <Card.Body>
-          <Text>A pressable card.</Text>
-        </Card.Body>
-      </Card>
+      <Grid.Container gap={2}>
+        {SpotlightProjects.map(project => (
+          <Grid xs={6} key={project.name}>
+            <Card
+              isHoverable variant={'bordered'}
+              isPressable
+              css={{
+                background: 'transparent',
+              }}
+              borderWeight={'bold'}
+            >
+              <Card.Body className={styles.projectCard}>
+                <Text h5>{project.name}</Text>
+                <Text
+                  small
+                  css={{
+                    color: '$accents7'
+                  }}
+                >
+                  {project.description}
+                </Text>
+                {project.icon && (
+                  <img src={project.icon} alt={project.name} className={styles.projectFigure}/>
+                )}
+              </Card.Body>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
     </div>
-  )
+  </>;
+  return div
 }
